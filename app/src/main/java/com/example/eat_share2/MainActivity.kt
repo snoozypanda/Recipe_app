05a1ac0dbd0f1e4167cff8adb383043d9ddec464
DanielJohn17 <danielyohannes38@gmail.com>
2025-06-07 17:50:45 +0300

    cleanup

--- app/src/main/java/com/example/eat_share2/MainActivity.kt
+++ app/src/main/java/com/example/eat_share2/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.eat_share2
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
